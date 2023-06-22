39a5be0c09aa0d46c4f86bfa9d82e94cd88e7a57
ritwick-roy <ritwickroy2002@gmail.com>
2023-06-23 01:53:31 +0530

    initial commit

---
+++ server/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const http = require('http');
+const { graphqlHTTP } = require('express-graphql');
+const schema = require('./schema/schema');
+const connectDB = require('./config/db');
+require('dotenv').config();
+
+const SERVER_PORT = process.env.SERVER_PORT || 5000;
+const app=express();
+
+connectDB();
+
+app.use('/graphql',graphqlHTTP({
+  schema,
+  graphiql:process.env.NODE_ENV==='development'
+}));
+
+const server = http.createServer(app);
+server.listen(SERVER_PORT,()=>(
+  console.log( `Server running on http://localhost:${SERVER_PORT}/`)
+));
